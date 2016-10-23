0ce8a0978e21a92b3dcaa4f1f8454d570bdee678
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2016-10-23 20:12:03 +0200

    added calculator for lesson 1

---
+++ livingdoc-training/src/main/java/info/novatec/testit/livingdoc/training/exercises/lessons/Calculator.java
@@ -0,0 +1,21 @@
+package info.novatec.testit.livingdoc.training.exercises.lessons;
+
+public class Calculator {
+
+    public float add(float a, float b) {
+        return a + b;
+    }
+
+    public float subtract(float a, float b) {
+        return a - b;
+    }
+
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+
+    public float divide(float a, float b) {
+        return a / b;
+    }
+
+}
